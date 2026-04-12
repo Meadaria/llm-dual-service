3ccce4155a62dc94cd3422a6306819ba76e4a46a
Daria Menshikova <dari.menshikova@yandex.ru>
2026-04-12 21:04:44 +0300

    project_final

--- bot_service/app/infra/celery_app.py
+++ bot_service/app/infra/celery_app.py
@@ -1,5 +1,4 @@
 from celery import Celery
-
 from app.core.config import settings
 
 celery_app = Celery(
@@ -15,9 +14,7 @@ celery_app.conf.update(
     timezone="UTC",
     enable_utc=True,
     task_track_started=True,
-    task_time_limit=30 * 60,
-    task_soft_time_limit=25 * 60,
-    result_expires=3600,
+    worker_pool="solo",
     task_acks_late=True,
     worker_prefetch_multiplier=1,
 )
